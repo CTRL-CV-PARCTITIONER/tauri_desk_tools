b578f3d1bef2cbe00b3476b546d99ecbcbb71b1c
weidong62 <weidong0624@163.com>
2023-05-04 17:41:59 +0800

    finish left task bar -- url

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,25 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
 
+
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
-
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
-  }
-
   return (
-    <div className="container">
-      <h1>Welcome to Tauri!</h1>
-
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <>
+      {/* 头部 */}
+      <h1 className="title">Tools</h1>
+      {/* 左侧隐藏式任务栏 */}
+      <div className="left_taskbar">
+        {/* 项目集成在此，新增项目可增加 <li>new project</li> */}
+        <ul className="project_items">
+          <a href="">
+            <li></li>
+          </a>
+          <a href="">
+            <li></li>
+          </a>
+        </ul>
       </div>
-
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <div className="row">
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            greet();
-          }}
-        >
-          <input
-            id="greet-input"
-            onChange={(e) => setName(e.currentTarget.value)}
-            placeholder="Enter a name..."
-          />
-          <button type="submit">Greet</button>
-        </form>
-      </div>
-
-      <p>{greetMsg}</p>
-    </div>
-  );
+    </>
+  )
 }
 
 export default App;
